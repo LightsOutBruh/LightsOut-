79077f4d488c26d838fd2ce7e5ab8c344006fe4e
dvluong <dvluong9218@gmail.com>
2015-05-28 10:19:43 -0700

    able to control Pi, zero bugs

--- LightsOut!/app/src/main/java/com/cs499/dvluong/lightsout/OnOffUI.java
+++ LightsOut!/app/src/main/java/com/cs499/dvluong/lightsout/OnOffUI.java
@@ -2,22 +2,20 @@ package com.cs499.dvluong.lightsout;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.transition.TransitionInflater;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.IOException;
 
-import android.transition.Transition;
-import android.transition.Scene;
-import android.transition.TransitionManager;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 
 public class OnOffUI extends Activity {
@@ -29,25 +27,69 @@ public class OnOffUI extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_off_ui);
-
         // button sound
         clickSound = MediaPlayer.create(this, R.raw.button_click);
         image = (ImageView)findViewById(R.id.onView);
-
         toggleButton = (Button) findViewById(R.id.toggleButton);
         toggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickSound.start();
-                if (image.getVisibility() == View.INVISIBLE){
+                if (image.getVisibility() == View.INVISIBLE) {
+                    new ConnectionOn().execute();
                     image.setVisibility(View.VISIBLE);
                 } else {
+                    new ConnectionOff().execute();
                     image.setVisibility(View.INVISIBLE);
                 }
-
             }
         });
 
+    }
+
+    private class ConnectionOn extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            connectOn();
+            return null;
+        }
+    }
+
+    private class ConnectionOff extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            connectOff();
+            return null;
+        }
+    }
+    private void connectOff() {
+        try {
+            DefaultHttpClient client = new DefaultHttpClient();
+
+            // This should be http://c14071c2.ngrok.io/off, but they are reversed
+            HttpGet request = new HttpGet("http://c14071c2.ngrok.io/on");
+
+            HttpResponse response = client.execute(request);
+        } catch (ClientProtocolException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        } catch (IOException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        }
+    }
+
+    private void connectOn() {
+        try {
+            DefaultHttpClient client = new DefaultHttpClient();
+
+            // This should be http://c14071c2.ngrok.io/on, but they are reversed
+            HttpGet request = new HttpGet("http://c14071c2.ngrok.io/off");
+
+            HttpResponse response = client.execute(request);
+        } catch (ClientProtocolException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        } catch (IOException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        }
     }
 
     @Override
